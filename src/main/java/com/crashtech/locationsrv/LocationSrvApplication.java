c1998c942c30036598db524c7b4ac7e745047308
Afsar <afsar.s33@gmail.com>
2023-04-27 10:05:41 +0530

    initial commit

---
+++ src/main/java/com/crashtech/locationsrv/LocationSrvApplication.java
@@ -0,0 +1,13 @@
+package com.crashtech.locationsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationSrvApplication.class, args);
+	}
+
+}
